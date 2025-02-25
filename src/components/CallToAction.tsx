b303d27f9ac47cdb88f9c3d5877ad9e2714e94e1
deepesh8216 <kumardeepesh542@gmail.com>
2025-02-26 00:34:06 +0530

    Adding All Animation on Each section

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,16 +1,49 @@
+"use client";
 import helixImage from "@/assets/images/helix2.png"
 import emojiStarImage from "@/assets/images/emojistar.png"
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end end']
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [-80, 80]);
+
   return (
-    <div className="bg-black text-white py-[72px] sm:py-24 text-center">
+    <div className="bg-black text-white py-[72px] sm:py-24 text-center"
+      ref={containerRef}>
       <div className="container max-w-xl relative">
-        <Image src={helixImage} alt="Helix Image" className="absolute top-6 left-[calc(100%+36px)]" />
-        <Image src={emojiStarImage} alt="Emoji Star Image" className="absolute -top-[120px] right-[calc(100%+24px)]"  />
+        <motion.div
+          style={{
+            translateY
+          }}
+        >
+          <Image
+            src={helixImage}
+            alt="Helix Image"
+            className="absolute top-6 left-[calc(100%+36px)]"
+          />
+        </motion.div>
+        <motion.div
+         style={{
+          translateY
+        }}
+        >
+          <Image
+            src={emojiStarImage}
+            alt="Emoji Star Image"
+            className="absolute -top-[120px] right-[calc(100%+24px)]"
+          />
+        </motion.div>
         <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">Get instant access</h2>
         <p className=" text-xl text-white/70 mt-5">
-        Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your success, one task at a time.
+          Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your success, one task at a time.
         </p>
         <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row">
           <input type="email" placeholder="your@email.com" className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"
--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,18 @@
+"use client";
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 export const ProductShowcase = () => {
+
+  const appImage = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ['start end', 'end end'],
+  });
+  const rotateX = useTransform(scrollYProgress, [0, 1], [25, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [.5, 1]);
   return (
     <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CAB] py-[72px] sm:py-24">
       <div className="container">
@@ -13,9 +24,21 @@ export const ProductShowcase = () => {
             Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your success, one task at a time.
           </p>
         </div>
-        <div className="flex justify-center mt-14">
-          <Image src={appScreen} alt="The product screenshot" className="mt-14" />
-        </div>
+        <motion.div className="flex justify-center mt-14"
+        style={{
+          opacity: opacity,
+          rotateX: rotateX,
+          transformPerspective: "800px",
+        }}
+        
+        >
+          <Image 
+          src={appScreen} 
+          alt="The product screenshot" 
+          className="mt-14" 
+          ref={appImage}
+          />
+        </motion.div>
       </div>
     </div>
   );
