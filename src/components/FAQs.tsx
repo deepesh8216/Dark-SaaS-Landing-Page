0492a367f5f173a0bee0e8adbd372c072accd66e
deepesh8216 <kumardeepesh542@gmail.com>
2025-02-25 22:47:21 +0530

    first commit

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,3 +1,9 @@
+"use client";
+import PlusIcon from "@/assets/icons/plus.svg"
+import React from "react";
+import clsx from "clsx";
+import MinusIcon from "@/assets/icons/minus.svg"
+
 const items = [
   {
     question: "What payment methods do you accept?",
@@ -21,6 +27,40 @@ const items = [
   },
 ];
 
+const AccordionItem = ({ question, answer }: { question: string; answer: string }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <div className="py-7 border-b border-white/30"
+    onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className="flex items-center">
+        <span className="flex-1 text-lg font-bold">{question}</span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+        
+      </div>
+      <div
+        className={clsx("mt-4", {
+          hidden: !isOpen,
+          "": isOpen === true,
+        })}
+      >
+        {answer}
+      </div>
+    </div>
+  )
+}
+
 export const FAQs = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-[#5D2CAB] to-black py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center text-5xl sm:text-6xl sm:max-w-[648px] mx-auto font-bold tracking-tighter">Frequently asked questions</h2>
+        <div className="mt-12 max-w-[648px] mx-auto">
+          {items.map(({ question, answer }) => (
+           <AccordionItem question={question} answer={answer} key={question} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CAB] py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center text-5xl sm:text-6xl font-bold tracking-tighter">Intuitive interface
+
+        </h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-xl text-center text-white/70 mt-5">
+            Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your success, one task at a time.
+          </p>
+        </div>
+        <div className="flex justify-center mt-14">
+          <Image src={appScreen} alt="The product screenshot" className="mt-14" />
+        </div>
+      </div>
+    </div>
+  );
 };
